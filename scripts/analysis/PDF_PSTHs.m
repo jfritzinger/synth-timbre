acc7a1e7dd9476d97e7f2ebb1003c6d5889b8ea7
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-03-21 17:01:20 -0400

    Added ISI, FFT, and ISI scatter plot for mode-locking

--- scripts/analysis/PDF_PSTHs.m
+++ scripts/analysis/PDF_PSTHs.m
@@ -12,7 +12,7 @@ sessions = readtable(fullfile(datapath, 'data-cleaning', ...
 num_data = size(sessions, 1);
 
 % Initialize report
-filename = 'PSTH_VS_all';
+filename = 'PSTH_VS_allsp';
 images = {}; %hold all plots as images, need to delete when finished
 datetime.setDefaultFormats('default','yyyy-MM-dd_hhmmss')
 report_name = sprintf('%s/pdfs/%s_%s.pdf', savepath, datetime, filename);
@@ -29,7 +29,7 @@ pm.PageMargins.Left = '0.2in';
 pm.PageMargins.Right = '0.2in';
 
 %% Plot each dataset
-
+warning('off')
 % Find sessions with MTF
 MTF_target = 'BS';
 isMTF = strcmp(sessions.MTF, MTF_target);
@@ -72,7 +72,7 @@ for isesh = 1:num_sessions
 
 		% Set up figure
 		fig = figure('Position',[292,274,1264,420]);
-		tiledlayout(3, 3, 'TileSpacing','compact', 'Padding','tight', ...
+		tiledlayout(4, 4, 'TileSpacing','compact', 'Padding','tight', ...
 			'TileIndexing','columnmajor')
 		x_label = [1000 2000 3000 4000 6000 8000]./1000;
 		fontsize = 10;
@@ -129,6 +129,41 @@ for isesh = 1:num_sessions
 		ylabel('Avg. Rate (sp/s)')
 		%legend('Unmodulated', 'Location','southwest')
 
+		% Plot synth timbre average rates
+		% Plot synthetic timbre
+		ileg = 1;
+		data_colors = {'#82BB95', '#3F985C', '#03882F', '#034E1C'};
+		for ispl = 2
+			if bin200_MTF(ineuron, ispl)==1
+				param_ST = data(5+ispl, 2);
+				data_ST = analyzeST(param_ST, CF);
+				data_ST = data_ST{1};
+
+				% Get CF rate
+				[~, CF_ind] = min(abs(CF-data_ST.fpeaks));
+				CF_rate = data_ST.rates_sm(CF_ind);
+
+				% Get spl
+				leg{ileg} = [num2str(param_ST{1}.spl) ' dB SPL'];
+				ileg = ileg + 1;
+
+				% Plot BE Synth Timbre
+				nexttile
+				hold on
+				plot(data_ST.fpeaks,data_ST.rates_sm, 'linewidth', 1.5,'Color',data_colors{ispl});
+				%plot(data_ST.fpeaks,data_ST.rate, 'linewidth', 1.5, 'Color',data_colors{ispl});
+			end
+		end
+		plot_range = [param_ST{1}.fpeaks(1) param_ST{1}.fpeaks(end)];
+		xline(CF, '--', 'Color',CF_color, 'linewidth', 1.5)
+		xlim(plot_range)	
+		xlabel('Frequency (Hz)')
+		ylabel('Avg. Rate (sp/s)')
+		box on
+		title('Synth Timbre: F0=200, Bin')
+		grid on
+		set(gca, 'fontsize', fontsize)
+
 		% Plot synthetic timbre
 		ileg = 1;
 		data_colors = {'#82BB95', '#3F985C', '#03882F', '#034E1C'};
@@ -151,7 +186,7 @@ for isesh = 1:num_sessions
 			ileg = ileg + 1;
 
 			% Plot BE Synth Timbre
-			nexttile(3)
+			nexttile
 			hold on
 			plot(param.fpeaks, temporal.VS)
 			num_fpeaks = length(param.fpeaks);
@@ -174,38 +209,38 @@ for isesh = 1:num_sessions
 
 		% Plot PSTH
 		freq_values = round(param.fpeaks);
-		if bin200_MTF(ineuron, ispl)==1
-			nexttile([3, 1])
-			hold on
-			max_rate = max(temporal.PSTH, [], 'all');
-			for j = 1:num_fpeaks
-
-				% Plot PSTHs
-				counts = temporal.PSTH(j,:);
-				edges = temporal.t;
-				t_bin = edges(1:end-1) + diff(edges)/2; % Bin centers
-				x_patch = repelem(edges, 2);
-				y_patch = repelem([0; counts(:); 0]', 2);
-				y_patch = y_patch(2:end-1); % Creates [0 y1 y1 0 0 y2 y2 0...]
-				offset = (j-1)*max_rate; % Adjust offset amount
-				patch(x_patch, y_patch + offset, 'b', 'FaceAlpha',0.5, 'EdgeColor','k');
-
-				% Plot smoothed PSTH
-				plot(temporal.t(1:end-1), temporal.PSTH_smooth(j,:)+offset,'k', 'LineWidth',1.5);
-
-			end
-			ylim([0 max_rate*num_fpeaks])
-			xlabel('Time (ms)')
-			box on
-			yticks(linspace(max_rate/2, max_rate*num_fpeaks-max_rate/2, num_fpeaks))
-			yticklabels(freq_values)
-			title('PSTH')
-			set(gca, 'fontsize', fontsize)
-		end
+		% if bin200_MTF(ineuron, ispl)==1
+		% 	nexttile([3, 1])
+		% 	hold on
+		% 	max_rate = max(temporal.PSTH, [], 'all');
+		% 	for j = 1:num_fpeaks
+		% 
+			% 	% Plot PSTHs
+			% 	counts = temporal.PSTH(j,:);
+			% 	edges = temporal.t;
+			% 	t_bin = edges(1:end-1) + diff(edges)/2; % Bin centers
+			% 	x_patch = repelem(edges, 2);
+			% 	y_patch = repelem([0; counts(:); 0]', 2);
+			% 	y_patch = y_patch(2:end-1); % Creates [0 y1 y1 0 0 y2 y2 0...]
+			% 	offset = (j-1)*max_rate; % Adjust offset amount
+			% 	patch(x_patch, y_patch + offset, 'b', 'FaceAlpha',0.5, 'EdgeColor','k');
+		% 
+			% 	% Plot smoothed PSTH
+			% 	plot(temporal.t(1:end-1), temporal.PSTH_smooth(j,:)+offset,'k', 'LineWidth',1.5);
+		% 
+		% 	end
+		% 	ylim([0 max_rate*num_fpeaks])
+		% 	xlabel('Time (ms)')
+		% 	box on
+		% 	yticks(linspace(max_rate/2, max_rate*num_fpeaks-max_rate/2, num_fpeaks))
+		% 	yticklabels(freq_values)
+		% 	title('PSTH')
+		% 	set(gca, 'fontsize', fontsize)
+		% end
 
 		% Plot period histogram
 		if bin200_MTF(ineuron, ispl)==1
-			nexttile([3, 1])
+			nexttile([4, 1])
 			max_rate = max(temporal.p_hist, [], 'all');
 			for j = 1:num_fpeaks
 
@@ -231,8 +266,139 @@ for isesh = 1:num_sessions
 			set(gca, 'fontsize', fontsize)
 		end
 
+		% Calculate ISI for each rep
+		nreps = param.nrep;
+		ISI_all = cell(1, num_fpeaks);
+		nbins = 30;
+		edges = linspace(0, 20, nbins+1); % Bin edges
+		counts_all = zeros(num_fpeaks, nbins); % Pre-allocate counts_all
+		for jj = 1:num_fpeaks
+			x = temporal.x{jj} / 1000; % ms
+			y = temporal.y{jj};
+
+			ISI = arrayfun(@(ii) diff(x(y == ii)), 1:nreps, 'UniformOutput', false);
+			ISI_all{jj} = vertcat(ISI{:});
+
+			counts_all(jj, :) = histcounts(ISI_all{jj}, edges);
+		end
+
+		% Plot ISI histograms
+		nexttile([4,1])
+		max_rate = max(counts_all, [], 'all');
+		freq_values = round(param.fpeaks);
+		for j = 1:num_fpeaks
+			counts = counts_all(j,:);
+
+			t_bin = edges(1:end-1) + diff(edges)/2; % Bin centers
+			x_patch = repelem(edges, 2);
+			y_patch = repelem([0; counts(:); 0]', 2);
+			y_patch = y_patch(2:end-1); % Creates [0 y1 y1 0 0 y2 y2 0...]
+			offset = (j-1)*max_rate; % Adjust offset amount
+			patch(x_patch, y_patch + offset, 'b', 'FaceAlpha',0.5, 'EdgeColor','k');
+		end
+		ylim([0 max_rate*num_fpeaks])
+		xlabel('Time (ms)')
+		box on
+		yticks(linspace(max_rate/2, max_rate*num_fpeaks-max_rate/2, num_fpeaks))
+		yticklabels(freq_values)
+		grid on
+		title('ISI Histogram')
+		set(gca, 'fontsize', fontsize)
+
+		%% 
+
+		temporal = analyzeST_Temporal(param, data_ST);
+		num_fpeaks = length(data_ST.fpeaks);
+		spike_hist = temporal.PSTH;
+		edges = temporal.t;
+		bin_width = (edges(2) - edges(1))/1000; % seconds
+		fs = round(1/bin_width);
+		[VS, fft_output, f] = calcFFT(spike_hist, param, fs);
+		nexttile([4, 1])
+		max_rate = max(fft_output, [], 'all');
+		freq_values = round(param.fpeaks);
+		hold on
+		for j = 1:num_fpeaks % Plot FFTs
+			offset = (j-1)*max_rate; % Adjust offset amount
+			plot(f, fft_output(j,:) + offset);
+		end
+		ylim([0 max_rate*num_fpeaks])
+		xlabel('Frequency (Hz)')
+		box on
+		yticks(linspace(max_rate/2, max_rate*num_fpeaks-max_rate/2, num_fpeaks))
+		yticklabels(freq_values)
+		grid on
+		title('FFT(PSTH)')
+		xlim([0 fs/2])
+		set(gca, 'fontsize', fontsize)
+
+		%%
+
 		% Add to PDF
 		[plt1, images] = addtoSTPDF(images, fig, putative);
+		append(rpt, plt1);
+
+		%% Plot ISI scatter plots
+		% Calculate ISI for each rep
+		ISI_all = cell(1, num_fpeaks);
+		counts_all = zeros(num_fpeaks, 50); % Pre-allocate counts_all
+		for jj = 1:num_fpeaks
+			ISI = [];
+			x_isi = [];
+			y_isi = [];
+			x = temporal.x{jj}/1000; % ms
+			y = temporal.y{jj};
+			for ii = 1:30
+				valid = y==ii;
+				isi = diff(x(valid));
+				ISI = [ISI isi'];
+				x_isi = [x_isi isi(1:end-1)'];
+				y_isi = [y_isi isi(2:end)'];
+			end
+			ISI_all{jj} = ISI;
+			x_isi_all{jj} = x_isi;
+			y_isi_all{jj} = y_isi;
+		end
+
+
+		fig = figure('Position',[560,42,1050,806]);
+		if num_fpeaks < 43
+			tiledlayout(6, 7, 'Padding','compact', 'TileSpacing','tight')
+			last_row = 36:41;
+		elseif num_fpeaks < 50
+			tiledlayout(7, 7, 'Padding','compact', 'TileSpacing','tight')
+			last_row = 42:49;
+		else
+			tiledlayout(8, 7, 'Padding','compact', 'TileSpacing','tight')
+			last_row = 50:56;
+		end
+		for jj = 1:num_fpeaks
+			T = 5; % period
+			nexttile
+			hold on
+			line(repmat([0;10*T],1,10),[1;1]*(1:10)*T,'Color','r', 'linewidth', 0.3)
+			line([1;1]*(1:10)*T,repmat([0;10*T],1,10),'Color','r', 'linewidth', 0.3)
+			axis square
+			scatter(x_isi_all{jj}, y_isi_all{jj},8, 'filled', 'k')
+
+			if ismember(jj, last_row)
+				xlabel('ISI n (ms)')
+			else
+				xticklabels([])
+			end
+
+			if ismember(jj, 1:7:41)
+				ylabel('ISI n+1 (ms)')
+			else
+				yticklabels([])
+			end
+			title(sprintf('%d Hz', freq_values(jj)))
+			xlim([0 30])
+			ylim([0 30])
+		end
+
+		% Add to PDF
+		[plt1, images] = addtoSTPDF(images, fig, [putative '_ISI']);
 		append(rpt, plt1);
 
 	end
@@ -264,4 +430,34 @@ img = Image(name);
 delete(fig) %delete plot figure window
 images = [images {img}];
 
+end
+
+%% FUNCTIONS 
+
+function [VS, fft_output, f] = calcFFT(spike_hist, params, fs)
+
+freq = 200;
+for ii = 1:length(params.fpeaks)
+
+	% Cut to integer # of cycles (50 cycles, excluding onset)
+	onsetwin = 0.05; % ms
+	spike_wo_onset = spike_hist(ii, round(onsetwin*fs):end);
+
+	% Find frequency index for 200 Hz
+	L = length(spike_wo_onset);
+
+	% Take FFT 
+	Y = fft(spike_wo_onset);
+	P2 = abs(Y)/L; % Normalize by signal length
+	P1 = P2(1:L/2+1);
+	P1(2:end-1) = 2*P1(2:end-1);
+	f = fs/L*(0:(L/2));
+
+	% Get FFT at 200 Hz 
+	ind = f==freq;
+	%VS(ii) = P1(ind);
+	fft_output(ii,:) = P1;
+	VS = 0;
+
+end
 end
