62a7690097ff7e7a711975b49cdb42b5631183ea
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-03-28 16:46:19 -0400

    Small changes

--- scripts/2025-manuscript/model_results.m
+++ scripts/2025-manuscript/model_results.m
@@ -132,113 +132,113 @@ for ii = 1:2
 end
 
 %% RMSE Plots 
-
-figure('position', [1796,680,7*ppi,3.2*ppi])
-fontsize = 12;
-subplot_numbers = [1, 4, 7; 10, 13, 16];
-labelsize = 20;
-
-h = gobjects(6, 1);
-isSPL = sessions.SPL == 63;
-for ii = 1:2
-
-	isBS = strcmp(sessions.MTF, 'BS');
-	isBE = strcmp(sessions.MTF, 'BE');
-	if ii == 1
-		x_R2 = sessions.Energy_rmse(isBS);
-		x_R22 = sessions.Energy_rmse(isBE);
-		
-		y_R2 = sessions.SFIE_rmse(isBS);
-		y_R22 = sessions.SFIE_rmse(isBE);
-	elseif ii == 2
-		x_R2 = sessions.Energy_rmse(isBS);
-		x_R22 = sessions.Energy_rmse(isBE);
-
-		y_R2 = sessions.Lat_Inh_rmse(isBS);
-		y_R22 = sessions.Lat_Inh_rmse(isBE);
-	else
-		x_R2 = sessions.SFIE_rmse(isBS);
-		x_R22 = sessions.SFIE_rmse(isBE);
-
-		y_R2 = sessions.Lat_Inh_rmse(isBS);
-		y_R22 = sessions.Lat_Inh_rmse(isBE);
-	end
-
-	h(subplot_numbers(1, ii)) = subplot(6, 3, subplot_numbers(1, ii)); % [1 4 7 10 13 16]
-	hold on
-	scatter(x_R2, y_R2, 20, 'filled', 'MarkerEdgeColor','k', "MarkerFaceAlpha",0.7)
-	scatter(x_R22, y_R22, 20, 'filled', 'MarkerEdgeColor','k', ...
-		'MarkerFaceColor',"#D95319", "MarkerFaceAlpha",0.7)
-	%scatter(x_non, y_non, 'MarkerEdgeColor','k')
-	plot([-1 90], [-1 90], 'k')
-	xticks(0:20:80)
-	yticks(0:20:80)
-	xticklabels([])
-	yticklabels([])
-	set(gca, 'fontsize', fontsize)
-	yline(0)
-	xline(0)
-	ylim([0 90])
-	xlim([0 90])
-	if ii == 1
-		legend('BS', 'BE', '', '', 'location', 'south')
-	end
-	grid on
-
-	% Create distribution plot for the X-axis (horizontal)
-	edges = linspace(0, 90, 20);
-	h(subplot_numbers(1, ii)+1) = subplot(6, 3, subplot_numbers(1, ii)+1);
-	histogram(x_R2,edges, 'Orientation', 'vertical', 'EdgeColor', 'k');
-	hold on
-	histogram(x_R22,edges, 'Orientation', 'vertical', 'EdgeColor', 'k', 'FaceColor', "#D95319");
-	xlim([0 90])
-	xlabel('Energy RMSE_n_o_r_m')
-	set(gca, 'fontsize', fontsize)
-	xticks(0:20:80)
-	yticks([])
-	grid on
-	box off
-
-	% Create distribution plot for the Y-axis (vertical) below the scatter plot
-	edges = linspace(0, 90, 20);
-	h(subplot_numbers(1, ii)+2) = subplot(6, 3, subplot_numbers(1, ii)+2);
-	histogram(y_R2,edges, 'Orientation', 'horizontal', 'EdgeColor', 'k');
-	hold on
-	histogram(y_R22,edges, 'Orientation', 'horizontal', 'EdgeColor', 'k', 'FaceColor', "#D95319");
-	ylim([0 90])
-	if ii == 1
-		ylabel('SFIE RMSE_n_o_r_m')
-	else
-		ylabel('Broad Inh. RMSE_n_o_r_m')
-	end
-	set(gca, 'fontsize', fontsize)
-	yticks(0:20:80)
-	xticks([])
-	grid on
-	box off
-end
-
-% Arrange plots 
-all_fig_positions = ...
-   [0.15,0.29,0.32,0.63;...
-	0.64,0.29,0.32,0.63]; % left bottom width height
-
-subplot_numbers = [1, 4];
-for ipos = 1:2
-	fig_position = all_fig_positions(ipos,:);
-	nb_position = [fig_position(1),fig_position(2)-0.12,fig_position(3),0.1];
-	wb_position = [fig_position(1)-0.05,fig_position(2),0.04,fig_position(4)];
-	set(h(subplot_numbers(ipos)), 'Position', fig_position)
-	set(h(subplot_numbers(ipos)+1), 'Position', nb_position)
-	set(h(subplot_numbers(ipos)+2), 'Position', wb_position)
-end
-
-% Annotate
-labels = {'C', 'D',};
-labelleft= repmat([0.01, 0.51], 1, 2);
-labelbottom = [repmat(0.95,1, 2) repmat(0.48, 1, 2)];
-for ii = 1:2
-	annotation('textbox',[labelleft(ii) labelbottom(ii) 0.071 0.058],...
-		'String',labels{ii},'FontWeight','bold','FontSize',labelsize,...
-		'EdgeColor','none');
-end
+% 
+% figure('position', [1796,680,7*ppi,3.2*ppi])
+% fontsize = 12;
+% subplot_numbers = [1, 4, 7; 10, 13, 16];
+% labelsize = 20;
+% 
+% h = gobjects(6, 1);
+% isSPL = sessions.SPL == 63;
+% for ii = 1:2
+% 
+% 	isBS = strcmp(sessions.MTF, 'BS');
+% 	isBE = strcmp(sessions.MTF, 'BE');
+% 	if ii == 1
+% 		x_R2 = sessions.Energy_rmse(isBS);
+% 		x_R22 = sessions.Energy_rmse(isBE);
+% 
+% 		y_R2 = sessions.SFIE_rmse(isBS);
+% 		y_R22 = sessions.SFIE_rmse(isBE);
+% 	elseif ii == 2
+% 		x_R2 = sessions.Energy_rmse(isBS);
+% 		x_R22 = sessions.Energy_rmse(isBE);
+% 
+% 		y_R2 = sessions.Lat_Inh_rmse(isBS);
+% 		y_R22 = sessions.Lat_Inh_rmse(isBE);
+% 	else
+% 		x_R2 = sessions.SFIE_rmse(isBS);
+% 		x_R22 = sessions.SFIE_rmse(isBE);
+% 
+% 		y_R2 = sessions.Lat_Inh_rmse(isBS);
+% 		y_R22 = sessions.Lat_Inh_rmse(isBE);
+% 	end
+% 
+% 	h(subplot_numbers(1, ii)) = subplot(6, 3, subplot_numbers(1, ii)); % [1 4 7 10 13 16]
+% 	hold on
+% 	scatter(x_R2, y_R2, 20, 'filled', 'MarkerEdgeColor','k', "MarkerFaceAlpha",0.7)
+% 	scatter(x_R22, y_R22, 20, 'filled', 'MarkerEdgeColor','k', ...
+% 		'MarkerFaceColor',"#D95319", "MarkerFaceAlpha",0.7)
+% 	%scatter(x_non, y_non, 'MarkerEdgeColor','k')
+% 	plot([-1 90], [-1 90], 'k')
+% 	xticks(0:20:80)
+% 	yticks(0:20:80)
+% 	xticklabels([])
+% 	yticklabels([])
+% 	set(gca, 'fontsize', fontsize)
+% 	yline(0)
+% 	xline(0)
+% 	ylim([0 90])
+% 	xlim([0 90])
+% 	if ii == 1
+% 		legend('BS', 'BE', '', '', 'location', 'south')
+% 	end
+% 	grid on
+% 
+% 	% Create distribution plot for the X-axis (horizontal)
+% 	edges = linspace(0, 90, 20);
+% 	h(subplot_numbers(1, ii)+1) = subplot(6, 3, subplot_numbers(1, ii)+1);
+% 	histogram(x_R2,edges, 'Orientation', 'vertical', 'EdgeColor', 'k');
+% 	hold on
+% 	histogram(x_R22,edges, 'Orientation', 'vertical', 'EdgeColor', 'k', 'FaceColor', "#D95319");
+% 	xlim([0 90])
+% 	xlabel('Energy RMSE_n_o_r_m')
+% 	set(gca, 'fontsize', fontsize)
+% 	xticks(0:20:80)
+% 	yticks([])
+% 	grid on
+% 	box off
+% 
+% 	% Create distribution plot for the Y-axis (vertical) below the scatter plot
+% 	edges = linspace(0, 90, 20);
+% 	h(subplot_numbers(1, ii)+2) = subplot(6, 3, subplot_numbers(1, ii)+2);
+% 	histogram(y_R2,edges, 'Orientation', 'horizontal', 'EdgeColor', 'k');
+% 	hold on
+% 	histogram(y_R22,edges, 'Orientation', 'horizontal', 'EdgeColor', 'k', 'FaceColor', "#D95319");
+% 	ylim([0 90])
+% 	if ii == 1
+% 		ylabel('SFIE RMSE_n_o_r_m')
+% 	else
+% 		ylabel('Broad Inh. RMSE_n_o_r_m')
+% 	end
+% 	set(gca, 'fontsize', fontsize)
+% 	yticks(0:20:80)
+% 	xticks([])
+% 	grid on
+% 	box off
+% end
+% 
+% % Arrange plots 
+% all_fig_positions = ...
+%    [0.15,0.29,0.32,0.63;...
+% 	0.64,0.29,0.32,0.63]; % left bottom width height
+% 
+% subplot_numbers = [1, 4];
+% for ipos = 1:2
+% 	fig_position = all_fig_positions(ipos,:);
+% 	nb_position = [fig_position(1),fig_position(2)-0.12,fig_position(3),0.1];
+% 	wb_position = [fig_position(1)-0.05,fig_position(2),0.04,fig_position(4)];
+% 	set(h(subplot_numbers(ipos)), 'Position', fig_position)
+% 	set(h(subplot_numbers(ipos)+1), 'Position', nb_position)
+% 	set(h(subplot_numbers(ipos)+2), 'Position', wb_position)
+% end
+% 
+% % Annotate
+% labels = {'C', 'D',};
+% labelleft= repmat([0.01, 0.51], 1, 2);
+% labelbottom = [repmat(0.95,1, 2) repmat(0.48, 1, 2)];
+% for ii = 1:2
+% 	annotation('textbox',[labelleft(ii) labelbottom(ii) 0.071 0.058],...
+% 		'String',labels{ii},'FontWeight','bold','FontSize',labelsize,...
+% 		'EdgeColor','none');
+% end
