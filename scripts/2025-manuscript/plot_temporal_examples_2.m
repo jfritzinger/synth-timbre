2debf007207b491d85735783c370b45f00f32ab7
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-03-28 16:48:06 -0400

    Initial commit

---
+++ scripts/2025-manuscript/plot_temporal_examples_2.m
@@ -0,0 +1,176 @@
+%% plot_temporal_examples
+clear
+%% Load in spreadsheet
+
+[base, datapath, savepath, ppi] = getPaths();
+sheetpath = 'data/2025-manuscript/data-cleaning';
+spreadsheet_name = 'PutativeTable.xlsx';
+sessions = readtable(fullfile(base, sheetpath, spreadsheet_name), 'PreserveVariableNames',true);
+
+%% Load in example
+
+figure('Position',[67,297,8.5*ppi,6*ppi])
+tiledlayout(4, 5, "TileSpacing","compact", 'TileIndexing','columnmajor')
+legsize = 9;
+fontsize = 11;
+labelsize = 18;
+
+%%
+ind = [1, 6, 11, 16, 2, 7, 12, 17, 3, 8, 13, 18, 4, 9, 14, 19, 5, 10, 15, 20];
+for ii = 1:20
+
+	switch ii
+		case 1
+			putative = 'R25_TT1_P8_N02'; % blurred at CF
+		case 2
+			putative = 'R25_TT1_P8_N01'; % blurred at CF
+		case 3
+			putative = 'R25_TT4_P7_N01'; % blurred at CF
+		case 4
+			putative = 'R29_TT4_P2_N03'; % blurred at CF
+		case 5
+			putative = 'R29_TT4_P2_N09'; % 2 peaks
+		case 6
+			putative = 'R29_TT1_P3_N02'; % 2 peaks
+		case 7
+			putative = 'R29_TT4_P2_N10'; % 2 peaks
+		case 8
+			%putative = 'R25_TT2_P9_N07'; % 2 peaks
+			%putative = 'R29_TT1_P3_N01';
+			%putative = 'R29_TT1_P2_N03';
+			putative = 'R29_TT1_P3_N05';
+		case 9
+			putative = 'R29_TT1_P2_N03'; % 2 peaks, odd
+		case 10
+			putative = 'R25_TT1_P8_N15'; %'R25_TT1_P8_N03'; % 2 peaks, odd
+		case 11
+			putative = 'R25_TT1_P8_N04'; %'R29_TT4_P2_N16'; % 2 peaks, odd
+		case 12
+			putative = 'R25_TT2_P9_N02'; % % 2 peaks, odd
+		case 13
+			putative = 'R29_TT3_P2_N05'; % multiple
+		case 14
+			putative = 'R29_TT4_P3_N04'; % multiple
+		case 15
+			putative = 'R29_TT3_P2_N04'; % multiple
+		case 16
+			putative = 'R25_TT3_P8_N06'; % multiple
+		case 17
+			putative = 'R29_TT4_P5_N02'; %'R24_TT1_P12_N01';
+		case 18
+			putative = 'R29_TT3_P5_N03'; % rate only
+		case 19
+			putative = 'R27_TT4_P8_N10';
+		case 20
+			putative = 'R27_TT2_P8_N05';
+	end
+	filename = sprintf('%s.mat', putative);
+	load(fullfile(datapath,'neural_data', filename)), 'data';
+	index = find(cellfun(@(s) strcmp(putative, s), sessions.Putative_Units));
+	CF = sessions.CF(index);
+	MTF_shape = sessions.MTF{index};
+
+	%% Analysis
+
+	% Synthetic timbre analysis
+	params = data(7, 2);
+	params = params(~cellfun(@isempty, params));
+	data_ST  = analyzeST(params, CF);
+	data_ST = data_ST{1};
+	param = params{1};
+	temporal = analyzeST_Temporal(param, data_ST);
+
+	% Vector strength calculation
+	fpeaks_re_CF{ii} = log2(param.fpeaks/CF);
+	VS{ii} = temporal.VS;
+
+	%% Plot
+
+	% Plot as heatmap
+	num_fpeaks = length(data_ST.fpeaks);
+	max_rate = max(temporal.p_hist, [], 'all');
+	freq_values = round(param.fpeaks);
+	p_hist = temporal.p_hist;
+	edges = temporal.t_hist;
+	t_bin = edges(1:end-1) + diff(edges)/2; % Bin centers
+	t = linspace(0, 5, size(p_hist,2));
+
+	grayMap = [linspace(0, 1, 256)', linspace(0, 1, 256)', linspace(0, 1, 256)'];
+	grayMap = flipud(grayMap);
+
+	h(ind(ii)) = subplot(4, 5, ind(ii));
+	hh = pcolor(t, data_ST.fpeaks./1000, p_hist);
+	set(hh, 'EdgeColor', 'none');
+	hold on
+	yline(CF/1000, 'r', 'LineWidth',2)
+	%colorbar;
+	%axis square;
+	colormap(grayMap);
+	xlim([0 5])
+	max_rate = max(p_hist, [], "all");
+	clim([0 max_rate-max_rate*0.3])
+	if ismember(ii, 3)
+		ylabel('                                Spectral Peak Freq. (Hz)')
+	end
+	xticks(0:5)
+	if ismember(ii, [4, 8, 12, 16, 20])
+		xlabel('Period (ms)')
+	else
+		xticklabels([])
+	end
+	box off
+
+	set(gca, 'fontsize', fontsize)
+	if ii == 17
+		hleg = legend('', 'CF', 'Location','northwest', 'fontsize', ...
+			legsize, 'box', 'off');
+		hleg.ItemTokenSize = [8, 8];
+	end
+
+	msg = sprintf('%d sp/s', max_rate);
+	text(0.58, 1, msg, 'Units', 'normalized', ...
+		'VerticalAlignment', 'top', 'FontSize',legsize, 'Color','r')
+
+
+end
+
+%% Set positions
+
+height = 0.19;
+width = 0.14;
+bottom = fliplr(linspace(0.1, 0.76, 4));
+left = linspace(0.07, 0.85, 5);
+
+left = repmat(left, 1, 4);
+bottom = reshape(repmat(bottom, 5, 1), 1, []);
+
+for ii = 1:20
+	set(h(ii), 'Position', [left(ii) bottom(ii) width height])
+end
+
+% Annotations
+left = linspace(0.02, 0.81, 5);
+annotation('textbox',[left(1) 0.965 0.0826 0.0385],'String',{'A'},...
+	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
+annotation('textbox',[left(2) 0.965 0.0826 0.0385],'String',{'B'},...
+	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
+annotation('textbox',[left(3) 0.965 0.0826 0.0385],'String',{'C'},...
+	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
+annotation('textbox',[left(4) 0.965 0.0826 0.0385],'String',{'D'},...
+	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
+annotation('textbox',[left(5) 0.965 0.0826 0.0385],'String',{'E'},...
+	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
+
+
+%% Plot vector strengths (then add back in later)
+
+
+for ineuron = 1:4
+
+	% Plot
+	for ii = 1:4
+		fpeaks_re_CF(ii,:) = log2(param.fpeaks/CF);
+		VS(ii,:) = temporal.VS;
+	end
+end
+
---
+++ scripts/2025-manuscript/supp_model_temporal.m
@@ -0,0 +1,87 @@
+%% supp_model_temporal
+clear 
+
+%% Parameters
+CF = 1200; 
+
+% Stimulus parameters
+params.fpeak_mid = 1200;
+params.Delta_F = 200;
+params.num_harms = 11;
+params.stp_otc = 1;
+params.Fs = 100000;
+params.mnrep = 1;
+params.physio = 0;
+params.dur = 0.3;
+params.ramp_dur = 0.02;
+params.spl = 70;
+params.g = 24;
+params = generate_ST(params);
+fs = params.Fs;
+
+%% Model 
+
+addpath '/Users/jfritzinger/Projects/synth-timbre/scripts/helper-functions'
+[base, datapath, ~] = getPaths();
+filename = 'Intro_ModelResponse.mat';
+load(fullfile(datapath, filename), 'AN_HSR', 'SFIE')
+avAN = AN_HSR.average_AN_sout;
+avBE = SFIE.average_ic_sout_BE;
+avBS = SFIE.average_ic_sout_BS;
+CFs = AN_HSR.CFs;
+
+%% Create heatmaps for BE and BS 
+
+figure('Position',[560,589,560,259])
+tiledlayout(1, 2, 'TileSpacing','compact', 'Padding','compact')
+for ii = 1:2
+	if ii == 1
+		spike_hist = squeeze(SFIE.ic_BE);
+		name = 'BE';
+	else
+		spike_hist = squeeze(SFIE.ic_BS);
+		name = 'BS';
+	end
+
+	% Calculate period histogram
+	nexttile
+	max_rate = max(spike_hist, [], 'all')/2;
+	onsetwin = 0.05;
+	hold on
+	for j = 1:100
+
+		% Plot PSTHs
+		spike_wo_onset = spike_hist(j, onsetwin*fs:params.dur*fs-1);
+		t = linspace(0.05, 0.3, fs*0.25);
+
+		freq = 200; % Stimulus frequency in Hz
+		period = 1 / freq; % Period in ms
+		samples_per_period = fs*period;
+
+		period_hist = reshape(spike_wo_onset, samples_per_period,[]);
+		avg(j,1:500) = mean(period_hist, 2)';
+		t_period = linspace(0, period, fs*period);
+
+	end
+	grayMap = [linspace(0, 1, 256)', linspace(0, 1, 256)', linspace(0, 1, 256)'];
+	grayMap = flipud(grayMap);
+
+	hh = pcolor(t_period, CFs./1000, avg);
+	set(hh, 'EdgeColor', 'none');
+	hold on
+	yline(CF/1000, 'r', 'LineWidth',2)
+	colormap(grayMap);
+	box on
+	yticks(linspace(max_rate/2, max_rate*100-max_rate/2, 100))
+	set(gca, 'YScale', 'log')
+	ylim([CFs(1) CFs(end)]/1000)
+	yticks([0.1 0.2 0.5 1 2 5 10])
+	ylabel('CFs (kHz)')
+	xlim([0 0.005])
+	xticks(0:0.001:0.005)
+	xticklabels(0:5)
+	xlabel('Period (ms)')
+	grid on
+	title(name)
+	set(gca, 'fontsize',12)
+end
