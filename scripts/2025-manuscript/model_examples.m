4b6bf72ab72b8101e7a23835a15041b5b56fb893
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-03-28 16:46:10 -0400

    Combined model_examples and model_results into one figure

--- scripts/2025-manuscript/model_examples.m
+++ scripts/2025-manuscript/model_examples.m
@@ -5,8 +5,8 @@ clear
 
 [base, datapath, savepath, ppi] = getPaths();
 
-figure('Position',[1720,621,4.7*ppi,7*ppi])
-h = gobjects(6, 1);
+figure('Position',[1720,621,8.5*ppi,7*ppi])
+h = gobjects(12, 1);
 
 fontsize = 11;
 legsize = 9;
@@ -61,14 +61,13 @@ for ii = 1:6
 
 	% Load in lateral inhibition model
 	load(fullfile(modelpath,'lat_inh_model', [putative '_Lat_Inh.mat']), 'lat_inh')
-	%load(fullfile(modelpath, 'model-fits', putative, ))
 
 	% Get spont rate
 	param_RM = data(2,2);
 	data_RM = analyzeRM(param_RM{1});
 
 	% Plot synthetic timbre (raw)
-	h(ii) = subplot(3, 2, ii);
+	h(ii) = subplot(3, 4, ii);
 	param_ST = data(5+ispl, 2);
 	if isempty(param_ST{1})
 		continue
@@ -103,20 +102,20 @@ for ii = 1:6
 	plot(data_ST.fpeaks, lat_inh_rate, 'LineWidth',linewidth, 'Color','#D55E00')
 
 	% Annotate SFIE model R^2
-	message = sprintf('R^2 SFIE = %.02f', SFIE{ispl}.R2);
-	text(0.05, 0.25, message, 'Units', 'normalized', ...
-		'VerticalAlignment', 'top', 'FontSize',legsize, 'Color',...
-		'#009E73')
-	% Annotate energy model R^2
-	message = sprintf('R^2 Energy = %.02f', energy{ispl}.R2);
-	text(0.05, 0.15, message, 'Units', 'normalized', ...
-		'VerticalAlignment', 'top', 'FontSize',legsize, 'Color',...
-		'#4634F1')
-	% Annotate lateral inhibition model R^2
-	message = sprintf('R^2 Broad inh = %.02f', lat_inh{ispl}.R2);
-	text(0.05, 0.35, message, 'Units', 'normalized', ...
-		'VerticalAlignment', 'top', 'FontSize',legsize, 'Color',...
-		'#D55E00')
+	% message = sprintf('R^2 SFIE = %.02f', SFIE{ispl}.R2);
+	% text(0.05, 0.25, message, 'Units', 'normalized', ...
+	% 	'VerticalAlignment', 'top', 'FontSize',legsize, 'Color',...
+	% 	'#009E73')
+	% % Annotate energy model R^2
+	% message = sprintf('R^2 Energy = %.02f', energy{ispl}.R2);
+	% text(0.05, 0.15, message, 'Units', 'normalized', ...
+	% 	'VerticalAlignment', 'top', 'FontSize',legsize, 'Color',...
+	% 	'#4634F1')
+	% % Annotate lateral inhibition model R^2
+	% message = sprintf('R^2 Broad inh = %.02f', lat_inh{ispl}.R2);
+	% text(0.05, 0.35, message, 'Units', 'normalized', ...
+	% 	'VerticalAlignment', 'top', 'FontSize',legsize, 'Color',...
+	% 	'#D55E00')
 
 	% Plot parameters
 	plot_range = [param_ST{1}.fpeaks(1) param_ST{1}.fpeaks(end)];
@@ -151,13 +150,106 @@ for ii = 1:6
 
 end
 
+%%
+
+% Load in spreadsheet
+[base, datapath, ~, ppi] = getPaths();
+spreadsheet_name = 'model_r2_values_ST2.xlsx';
+sessions = readtable(fullfile(datapath, spreadsheet_name), 'PreserveVariableNames',true);
+num_data = size(sessions, 1);
+subplot_numbers = [7, 10];
+isSPL = sessions.SPL == 63;
+for ii = 1:2
+
+	isBS = strcmp(sessions.MTF, 'BS');
+	isBE = strcmp(sessions.MTF, 'BE');
+	if ii == 1
+		isnotsig = sessions.p_s_e>0.05;
+		x_R2 = sessions.Energy_R(isBS & ~isnotsig);
+		x_R22 = sessions.Energy_R(isBE & ~isnotsig);
+		x_non = sessions.Energy_R(isnotsig);
+		
+		y_R2 = sessions.SFIE_R(isBS & ~isnotsig);
+		y_R22 = sessions.SFIE_R(isBE & ~isnotsig);
+		y_non = sessions.SFIE_R(isnotsig);
+	elseif ii == 2
+
+		isnotsig = sessions.p_l_e>0.05;
+		x_R2 = sessions.Energy_R(isBS & ~isnotsig);
+		x_R22 = sessions.Energy_R(isBE & ~isnotsig);
+		x_non = sessions.Energy_R(isnotsig);
+
+		y_R2 = sessions.Lat_Inh_R(isBS & ~isnotsig);
+		y_R22 = sessions.Lat_Inh_R(isBE & ~isnotsig);
+		y_non = sessions.Lat_Inh_R(isnotsig);
+	else
+
+		isnotsig = sessions.p_l_s>0.05;
+		x_R2 = sessions.SFIE_R(isBS & ~isnotsig);
+		x_R22 = sessions.SFIE_R(isBE & ~isnotsig);
+		x_non = sessions.SFIE_R(isnotsig);
+
+		y_R2 = sessions.Lat_Inh_R(isBS & ~isnotsig);
+		y_R22 = sessions.Lat_Inh_R(isBE & ~isnotsig);
+		y_non = sessions.Lat_Inh_R(isnotsig);
+
+	end
+
+	h(subplot_numbers(1, ii)) = subplot(3, 4, subplot_numbers(1, ii)); % [1 4 7 10 13 16]
+	hold on
+	scatter(x_R2, y_R2, 20, 'filled', 'MarkerEdgeColor','k', "MarkerFaceAlpha",0.7)
+	scatter(x_R22, y_R22, 20, 'filled', 'MarkerEdgeColor','k', ...
+		'MarkerFaceColor',"#D95319", "MarkerFaceAlpha",0.7)
+	%scatter(x_non, y_non, 'MarkerEdgeColor','k')
+	plot([-1 1], [-1 1], 'k')
+	xticklabels([])
+	yticklabels([])
+	set(gca, 'fontsize', fontsize)
+	yline(0)
+	xline(0)
+	if ii == 2
+		legend('BS', 'BE', '', '', 'location', 'south')
+	end
+
+	% Create distribution plot for the X-axis (horizontal)
+	edges = linspace(-1, 1, 20);
+	h(subplot_numbers(1, ii)+1) = subplot(3, 4, subplot_numbers(1, ii)+1);
+	histogram(x_R2,edges, 'Orientation', 'vertical', 'EdgeColor', 'k');
+	hold on
+	histogram(x_R22,edges, 'Orientation', 'vertical', 'EdgeColor', 'k', 'FaceColor', "#D95319");
+	xlim([-1 1])
+	xlabel('Energy R')
+	set(gca, 'fontsize', fontsize)
+	xticks(-1:0.5:1)
+	xline(0, 'k')
+	yticks([])
+	box off
+
+	% Create distribution plot for the Y-axis (vertical) below the scatter plot
+	edges = linspace(-1, 1, 20);
+	h(subplot_numbers(1, ii)+2) = subplot(3, 4, subplot_numbers(1, ii)+2);
+	histogram(y_R2,edges, 'Orientation', 'horizontal', 'EdgeColor', 'k');
+	hold on
+	histogram(y_R22,edges, 'Orientation', 'horizontal', 'EdgeColor', 'k', 'FaceColor', "#D95319");
+	ylim([-1 1])
+	if ii == 1
+		ylabel('SFIE R')
+	else
+		ylabel('Broad Inhibition R')
+	end
+	set(gca, 'fontsize', fontsize)
+	yticks(-1:0.5:1)
+	yline(0, 'k')
+	xticks([])
+	box off
+end
 
 %% Arrange
 
-left = [0.13 0.6];
+left = [0.07 0.32];
 bottom = linspace(0.1, 0.68, 3);
 height = 0.22;
-width = 0.35;
+width = 0.19;
 
 set(h(1), 'Position', [left(1) bottom(3) width height])
 set(h(2), 'Position', [left(2) bottom(3) width height])
@@ -167,7 +259,7 @@ set(h(5), 'Position', [left(1) bottom(1) width height])
 set(h(6), 'Position', [left(2) bottom(1) width height])
 
 % Annotate 
-left = [0.01 0.51];
+left = [0.01 0.275];
 bottom = linspace(0.32, 0.91, 3);
 annotation('textbox',[left(1) 0.97 0.0826 0.0385],'String',{'A'},...
 	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
@@ -181,3 +273,28 @@ annotation('textbox',[left(1) bottom(1) 0.0826 0.0385],'String',{'E'},...
 	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
 annotation('textbox',[left(2) bottom(1) 0.0826 0.0385],'String',{'F'},...
 	'FontWeight','bold','FontSize',labelsize,'EdgeColor','none');
+
+%%
+% Arrange plots 
+all_fig_positions = ...
+   [0.67,0.18,0.3,0.3;...
+	0.67,0.65,0.3,0.3]; % left bottom width height
+
+subplot_numbers = [7, 10];
+for ipos = 1:2
+	fig_position = all_fig_positions(ipos,:);
+	nb_position = [fig_position(1),fig_position(2)-0.08,fig_position(3),0.06];
+	wb_position = [fig_position(1)-0.06,fig_position(2),0.05,fig_position(4)];
+	set(h(subplot_numbers(ipos)), 'Position', fig_position)
+	set(h(subplot_numbers(ipos)+1), 'Position', nb_position)
+	set(h(subplot_numbers(ipos)+2), 'Position', wb_position)
+end
+
+% Annotate
+labels = {'G', 'H',};
+labelbottom = [0.95 0.48];
+for ii = 1:2
+	annotation('textbox',[0.55 labelbottom(ii) 0.071 0.058],...
+		'String',labels{ii},'FontWeight','bold','FontSize',labelsize,...
+		'EdgeColor','none');
+end
