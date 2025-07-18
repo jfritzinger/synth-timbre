1339cc5d53351c5d5e1ea9d2652cdf0a338e7fab
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-07-18 15:35:44 -0400

    Added VS calculations

--- scripts/helper-functions/analyzeST_Temporal.m
+++ scripts/helper-functions/analyzeST_Temporal.m
@@ -30,7 +30,6 @@ for i_fpeak = 1:num_fpeaks
 
 	% Example spike times (300 ms window, 200 Hz stimulus)
 	spike_times = x/1000;
-	freq = 200;
 
 	% Exclude 50ms onset
 	spike_times(spike_times<50) = [];
@@ -39,12 +38,31 @@ for i_fpeak = 1:num_fpeaks
 	period = 1000 / freq;
 	phases = 2 * pi * mod(spike_times, period) / period;
 	VS = abs(mean(exp(1i * phases)));
+	if ~isempty(phases)
+		p_value = circ_rtest(phases); % Rayleigh statistic (P < 0.01)
+	else
+		p_value = NaN;
+	end
 	%vectors = exp(-1i*2*pi*200*spike_times/1000); % same equation
 	%sync(j) = abs(mean(vectors)); % same equation
 
 	period = 1000 / 400;
 	phases = 2 * pi * mod(spike_times, period) / period;
 	VS_400 = abs(mean(exp(1i * phases)));
+
+	% Calculate phase locking to harmonics 
+	for iharm = 1:10
+		harm = freq+freq*(iharm-1);
+		period = 1000 / harm; % Get period of each harmonic
+		phases = 2 * pi * (mod(spike_times, period) / period);
+		VS_harms(iharm) = abs(mean(exp(1i * phases)));
+		if ~isempty(phases)
+			p_value_harms(iharm) = circ_rtest(phases); % Rayleigh statistic (P < 0.01)
+		else
+			p_value_harms(iharm) = NaN;
+		end
+		harms(iharm) = harm;
+	end
 
 	% Save outputs
 	temporal.x{i_fpeak} = x;
@@ -55,7 +73,11 @@ for i_fpeak = 1:num_fpeaks
 	temporal.p_hist(i_fpeak,:) = counts;
 	temporal.t_hist = edges;
 	temporal.VS(i_fpeak) = VS;
+	temporal.VS_p(i_fpeak) = p_value;
 	temporal.VS_400(i_fpeak) = VS_400;
+	temporal.VS_harms(i_fpeak,:) = VS_harms;
+	temporal.harms(i_fpeak,:) = harms;
+	temporal.VS_p_harms(i_fpeak,:) = p_value_harms;
 end
 
 end
