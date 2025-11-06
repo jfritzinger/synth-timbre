a7c6555e024e0b691fcb0bdad0e1458898917841
Johanna Fritzinger <johanna.fritzinger@gmail.com>
2025-11-06 09:57:26 -0500

    Added .tif file to save

--- scripts/helper-functions/save_figure.m
+++ scripts/helper-functions/save_figure.m
@@ -2,8 +2,8 @@ function save_figure(filename)
 
 [~, ~, savepath, ~] = getPaths();
 print(fullfile(savepath, [filename '.png']),'-dpng', '-r600')
-% print('-vector', fullfile(savepath, '2025-manuscript', [filename '.tif']),...
-% 	'-dtiff', '-r600')
+print('-vector', fullfile(savepath, '2025-manuscript', [filename '.tif']),...
+	'-dtiff', '-r600')
 % print('-vector', fullfile(savepath, '2025-manuscript', [filename '.eps']),...
 % 	'-depsc', '-r600')
 
